dd6cf712d6df9636450c2e1ae8258808a692f611
Andre Felipe <afelipes@gmail.com>
2022-12-27 18:18:39 -0300

    refactor users - model && migrations

--- backend/src/database/migrations/20221227031824-create-cnpjs.js
+++ backend/src/database/migrations/20221227031824-create-cnpjs.js
@@ -4,29 +4,11 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('cnpjs', {
-      id: {
-        allowNull: false, 
-        autoIncrement: true, 
-        primaryKey: true, 
-        type: Sequelize.INTEGER},
-      cnpj: {
-        allowNull: false, 
-        unique: true, 
-        type: Sequelize.STRING
-      },
-      companyType: {
-        allowNull: false,
-        field: 'company_type', 
-        type: Sequelize.STRING},
-      createdAt: {
-        allowNull: false,
-        field: 'created_at',
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false, 
-        field: 'updated_at',
-        type: Sequelize.DATE},
+      id: {allowNull: false, autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER},
+      cnpj: {allowNull: false, unique: true, type: Sequelize.STRING},
+      companyType: {allowNull: false, field: 'company_type', type: Sequelize.STRING},
+      createdAt: {allowNull: false, field: 'created_at', type: Sequelize.DATE},
+      updatedAt: {allowNull: false, field: 'updated_at', type: Sequelize.DATE},
     }); 
   },
 
--- backend/src/database/models/users.js
+++ backend/src/database/models/users.js
@@ -1,32 +1,15 @@
-// `id` int(11) NOT NULL,
-// `name` varchar(255) NOT NULL,
-// `email` varchar(255) NOT NULL,
-// `phoneNumber` varchar(255) DEFAULT NULL,
-// `mobile` varchar(255) DEFAULT NULL,
-// `departament` varchar(255) DEFAULT NULL,
-// `verificationCode` varchar(255) DEFAULT NULL,
-// `emailChecked` tinyint(1) DEFAULT 0,
-// `createdAt` datetime NOT NULL,
-// `updatedAt` datetime NOT NULL,
-// `cashforceAdm` tinyint(1) DEFAULT 0
-
-// ADD PRIMARY KEY (`id`),
-// ADD UNIQUE KEY `email` (`email`);
-
 const { DataTypes } = require('sequelize');
 
 const attributes = {
   id: {allowNull: false, autoIncrement: true, primaryKey: true, type: DataTypes.INTEGER},
-  name: {allowNull: false, type: DataTypes.STRING},
-  email: {allowNull: false, unique: true, type: DataTypes.STRING},
-  phoneNumber: {allowNull: true, type: DataTypes.STRING},
-  mobile: {allowNull: true, type: DataTypes.STRING},
-  departament: {allowNull: true, type: DataTypes.STRING},
-  verificationCode: {allowNull: true, type: DataTypes.STRING},
-  emailChecked: {allowNull: false, defaultValue: false, type: DataTypes.BOOLEAN},
-  createdAt: {allowNull: false, type: DataTypes.DATE},
-  updatedAt: {allowNull: false, type: DataTypes.DATE},
-  cashforceAdm: {allowNull: false, defaultValue: false, type: DataTypes.BOOLEAN},
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+  phoneNumber: DataTypes.STRING,
+  mobile: DataTypes.STRING,
+  departament: DataTypes.STRING,
+  verificationCode: DataTypes.STRING,
+  emailChecked: DataTypes.BOOLEAN,
+  cashforceAdm: DataTypes.BOOLEAN,
 };
 
 module.exports = (sequelize) => {
