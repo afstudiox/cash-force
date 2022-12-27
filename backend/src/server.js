b4bfa895fb059181536e7a906b9403185f37f62b
Andre Felipe <afelipes@gmail.com>
2022-12-26 22:51:58 -0300

    feat: making settings in .sequelizerce folder structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const app = require('./app');
-const PORT = 3001;
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
