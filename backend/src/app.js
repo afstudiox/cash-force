374c22f6111d808d0891bae534edb54cca0a4994
Andre Felipe <afelipes@gmail.com>
2022-12-29 21:41:57 -0300

    install cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const userRoute = require('./routes/userRoute');
 const orderRoute = require('./routes/orderRoute');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
